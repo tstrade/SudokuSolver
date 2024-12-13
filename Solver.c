a80b68453014e629b6adef0e0b6bd865aa70dc81
tstrade <99762399+tstrade@users.noreply.github.com>
2024-12-12 22:21:35 -0500

    Creater Solver.h - weird error with Solver.c (struct Solver definition?)

--- Solver.c
+++ Solver.c
@@ -1,17 +1,7 @@
 #include "CSP.h"
 #include "datastructs.h"
+#include "Solver.c"
 #include <stdlib.h>
-
-typedef struct Solver Solver;
-typedef struct AC3_args AC3_args;
-int var_AC3(AC3_args args);
-int AC3(Solver *self, CSP *csp, Queue *q, int **removals);
-int revise(struct Solver *self, CSP *csp, int Xi, int Xj, int **removals);
-void backtracking_search(Solver *self, CSP *csp);
-int *backtrack(Solver *self, int *assignment, CSP *csp);
-int select_unassigned_variable(Solver *self, int *assignment, CSP *csp);
-Queue *get_queue(Solver *self, CSP *csp, int variable);
-
 
 struct Solver {
   int (*var_AC3)(AC3_args args);
